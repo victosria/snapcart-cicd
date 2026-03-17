2fb3279e0b20980a87e3c9ee16fec3a4cb34dcc1
victosria <victoriascottimasiero@hotmail.com>
2026-03-17 14:49:04 -0700

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import "../styles/Home.module.css";
+import '../styles/globals.css'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
